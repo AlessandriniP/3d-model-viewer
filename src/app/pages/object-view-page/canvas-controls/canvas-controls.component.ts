d1c16dca7d5c6cc4ec9efc2f8eddd31b639ea652
Philipp Alessandrini <philipp.alessandrini@t-online.de>
2025-06-28 14:48:19 +0200

    Add tooltips for canvas controls

--- src/app/pages/object-view-page/canvas-controls/canvas-controls.component.ts
+++ src/app/pages/object-view-page/canvas-controls/canvas-controls.component.ts
@@ -2,11 +2,12 @@ import { AfterViewInit, Component, ElementRef, ViewChild, effect, inject, viewCh
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { UnityCommunicatorService } from '../../../services/unity-communicator.service';
 
 @Component({
   selector: 'app-canvas-controls',
-  imports: [MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [MatButtonModule, MatIconModule, MatDividerModule, MatTooltipModule],
   templateUrl: './canvas-controls.component.html',
   styleUrl: './canvas-controls.component.scss'
 })
