234135302590496d727a04cfe82962cecd9fde5b
Philipp Alessandrini <philipp.alessandrini@t-online.de>
2025-07-18 23:58:57 +0200

    Match unity webgl to canvas size only after window resizing

--- src/app/pages/object-view-page/unity-canvas/unity-canvas.component.ts
+++ src/app/pages/object-view-page/unity-canvas/unity-canvas.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Signal, ViewChild, inject, input, output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, Signal, ViewChild, inject, input, output, viewChild } from '@angular/core';
 import { UnityCommunicatorService } from '../../../services/unity-communicator.service';
 
 @Component({
@@ -7,18 +7,18 @@ import { UnityCommunicatorService } from '../../../services/unity-communicator.s
   templateUrl: './unity-canvas.component.html',
   styleUrl: './unity-canvas.component.scss'
 })
-export class UnityCanvasComponent implements AfterViewInit {
-  @ViewChild('unityCanvas') canvasRef!: ElementRef<HTMLCanvasElement>;
-
+export class UnityCanvasComponent implements AfterViewInit, OnDestroy {
+  canvasRef = viewChild.required<ElementRef<HTMLCanvasElement>>('unityCanvas');
   canvasReady = output<void>();
 
   readonly unityCanvasId = 'unity-canvas';
   readonly unityBuildPath = 'unity-build/unity-build';
 
   private readonly unityCommunicatorService = inject(UnityCommunicatorService);
+  private resizeHandler?: () => void;
 
   ngAfterViewInit(): void {
-    const unityCanvas = this.canvasRef.nativeElement;
+    const unityCanvas = this.canvasRef().nativeElement;
 
     try {
       // @ts-ignore
@@ -34,10 +34,37 @@ export class UnityCanvasComponent implements AfterViewInit {
         unityCanvas.style.cursor = 'crosshair';
         this.unityCommunicatorService.init(instance);
         this.canvasReady.emit();
+
+        this.registerWebGLToCanvasResizeHandler(instance);
       });
     }
     catch {
       // ignore exceptions
     }
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeHandler) {
+      window.removeEventListener('resize', this.resizeHandler);
+    }
+  }
+
+  private registerWebGLToCanvasResizeHandler(unityInstance: any): void {
+    const unityModule = unityInstance.Module;
+    let resizeTimeout: number;
+
+    this.resizeHandler = () => {
+      if (unityModule) {
+        unityModule.matchWebGLToCanvasSize = false;
+      }
+      clearTimeout(resizeTimeout);
+      resizeTimeout = window.setTimeout(() => {
+        if (unityModule) {
+          unityModule.matchWebGLToCanvasSize = true;
+        }
+      }, 100);
+    };
+
+    window.addEventListener('resize', this.resizeHandler);
   }
 }
